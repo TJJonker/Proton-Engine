608efa86cf090478f92a0996a57eb62857c1cecb
Tom Jonker <tjj.jonker@gmail.com>
2024-03-07 20:26:26 +0100

    Created IndexBuffer base class

---
+++ Proton Engine/src/Rendering/IndexBuffer.cpp
@@ -0,0 +1,9 @@
+#include "ppch.h"
+#include "Rendering/IndexBuffer.h"
+
+namespace Proton::Rendering {
+    IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size)
+    {
+        return nullptr;
+    }
+}
